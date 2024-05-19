94367bc0780f011ce879dbbd7a7008c3674649c9
Diego Martínez <a353198@uach.mx>
2024-05-19 04:55:32 -0600

    * main.py (CR)

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
-import cx_Oracle
+import re
+import oracledb
 
-## CONEXIÓN ##
-def connect_to_oracle():
+def conexion():
     usuario = "C##SERVERA"
     contraseña = "ADMIN"
     host = "localhost"
@@ -11,17 +11,17 @@ def connect_to_oracle():
     servicio = "FREE"
     try:
         # Establecer la conexión a la base de datos
-        connection = cx_Oracle.connect(usuario, contraseña, host + ':' + puerto + '/' + servicio)
+        connection = oracledb.connect(user=usuario, password=contraseña, dsn=f"{host}:{puerto}/{servicio}")
         print("Conexión exitosa a la base de datos.")
         return connection
-    except cx_Oracle.Error as error:
+    except oracledb.DatabaseError as error:
         print("Error al conectar a la base de datos:", error)
         return None
 
 # Crear la ventana principal
 root = tk.Tk()
-root.title("CRUD Interface")
-root.geometry("250x400")
+root.title("Proyecto 2")
+root.geometry("1430x400")
 # Crear el notebook principal
 main_notebook = ttk.Notebook(root)
 main_notebook.pack(fill='both', expand=True)
@@ -92,20 +92,20 @@ products_notebook.add(products_update, text="Update")
 products_delete = ttk.Frame(products_notebook)
 products_notebook.add(products_delete, text="Delete")
 
-# CUSTOMERS (CREATE)
+# =================================== CUSTOMERS (CREATE) =================================== #
 
 def create_customer():
-    customer_id = customer_id_entry.get()
-    nombre = nombre_entry.get()
-    apellido = apellido_entry.get()
-    credito = credito_entry.get()
-    correo = correo_entry.get()
-    ingresos = ingresos_entry.get()
-    region = region_combobox.get()
+    customer_id = C_C_CustomerID.get()
+    nombre = C_C_Nombre.get()
+    apellido = C_C_Apellido.get()
+    credito = C_C_Credito.get()
+    correo = C_C_Correo.get()
+    ingresos = C_C_Ingresos.get()
+    region = C_C_Region.get()
     try:
         
         # Conectarse a Oracle
-        connection = connect_to_oracle()
+        connection = conexion()
         if connection is None:
             return
 
@@ -122,44 +122,133 @@ def create_customer():
 
         print("Cliente creado exitosamente")
 
-    except cx_Oracle.DatabaseError as e:
-        error, = e.args
-        print("Error al conectar a Oracle:", error.message)
-
+    except oracledb.DatabaseError as e:
+        error = e.args[0]
+        print("Error al conectar a Oracle:", error)
 
 tk.Label(customers_create, text="Customer ID:").grid(row=0, column=0, padx=10, pady=10, sticky='e')
-customer_id_entry = tk.Entry(customers_create)
-customer_id_entry.grid(row=0, column=1, padx=10, pady=10, sticky='w')
+C_C_CustomerID = tk.Entry(customers_create)
+C_C_CustomerID.grid(row=0, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Nombre:").grid(row=1, column=0, padx=10, pady=10, sticky='e')
-nombre_entry = tk.Entry(customers_create)
-nombre_entry.grid(row=1, column=1, padx=10, pady=10, sticky='w')
+C_C_Nombre = tk.Entry(customers_create)
+C_C_Nombre.grid(row=1, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Apellido:").grid(row=2, column=0, padx=10, pady=10, sticky='e')
-apellido_entry = tk.Entry(customers_create)
-apellido_entry.grid(row=2, column=1, padx=10, pady=10, sticky='w')
+C_C_Apellido = tk.Entry(customers_create)
+C_C_Apellido.grid(row=2, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Crédito:").grid(row=3, column=0, padx=10, pady=10, sticky='e')
-credito_entry = tk.Entry(customers_create)
-credito_entry.grid(row=3, column=1, padx=10, pady=10, sticky='w')
+C_C_Credito = tk.Entry(customers_create)
+C_C_Credito.grid(row=3, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Correo:").grid(row=4, column=0, padx=10, pady=10, sticky='e')
-correo_entry = tk.Entry(customers_create)
-correo_entry.grid(row=4, column=1, padx=10, pady=10, sticky='w')
+C_C_Correo = tk.Entry(customers_create)
+C_C_Correo.grid(row=4, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Ingresos:").grid(row=5, column=0, padx=10, pady=10, sticky='e')
-ingresos_entry = tk.Entry(customers_create)
-ingresos_entry.grid(row=5, column=1, padx=10, pady=10, sticky='w')
+C_C_Ingresos = tk.Entry(customers_create)
+C_C_Ingresos.grid(row=5, column=1, padx=10, pady=10, sticky='w')
 
 tk.Label(customers_create, text="Región:").grid(row=6, column=0, padx=10, pady=10, sticky='e')
-region_combobox = ttk.Combobox(customers_create, values=["A", "B", "C", "D"], width=5, state="readonly")
-region_combobox.grid(row=6, column=1, padx=10, pady=10, sticky='w')
-region_combobox.set("A")
+C_C_Region = ttk.Combobox(customers_create, values=["A", "B", "C", "D"], width=5, state="readonly")
+C_C_Region.grid(row=6, column=1, padx=10, pady=10, sticky='w')
+C_C_Region.set("A")
 
-submit_button = tk.Button(customers_create, text="CREAR", command=create_customer)
-submit_button.grid(row=7, column=0, columnspan=2, pady=20)
+C_C_BTNCrear = tk.Button(customers_create, text="CREAR", command=create_customer)
+C_C_BTNCrear.grid(row=7, column=0, columnspan=2, pady=20)
     
-##############################################################################################
+# ========================================================================================== #
 
-# Ejecutar el bucle principal de la ventana
+# ==================================== CUSTOMERS (READ) ==================================== #
+def list_all_customers():
+    try:
+        connection = conexion()
+        if connection is None:
+            return
+
+        cursor = connection.cursor()
+
+        p_cursor = cursor.var(oracledb.CURSOR)
+
+        # Ejecutar el procedimiento almacenado
+        cursor.callproc("list_all_customers", [p_cursor])
+
+        result = p_cursor.getvalue().fetchall()
+
+        return result
+
+    except oracledb.DatabaseError as e:
+        error = e.args[0]
+        print("Error al conectar a Oracle:", error)
+        return None
+    
+def show_customers():
+    # Obtener los clientes
+    customers = list_all_customers()
+
+    if customers:
+        # Limpiar la tabla
+        for row in C_R_Tabla.get_children():
+            C_R_Tabla.delete(row)
+
+        # Agregar los clientes a la tabla
+        for customer in customers:
+            C_R_Tabla.insert("", "end", values=customer)
+
+# Tabla de Clientes
+C_R_Tabla = ttk.Treeview(customers_read, columns=("Customer ID", "Nombre", "Apellido", "Crédito", "Correo", "Ingresos", "Región"), show="headings")
+C_R_Tabla.heading("Customer ID", text="Customer ID")
+C_R_Tabla.heading("Nombre", text="Nombre")
+C_R_Tabla.heading("Apellido", text="Apellido")
+C_R_Tabla.heading("Crédito", text="Crédito")
+C_R_Tabla.heading("Correo", text="Correo")
+C_R_Tabla.heading("Ingresos", text="Ingresos")
+C_R_Tabla.heading("Región", text="Región")
+C_R_Tabla.grid(row=0, column=0, sticky="nsew")
+scrollbar = ttk.Scrollbar(customers_read, orient="vertical", command=C_R_Tabla.yview)
+scrollbar.grid(row=0, column=1, sticky="ns")
+C_R_Tabla.configure(yscrollcommand=scrollbar.set)
+
+load_button = ttk.Button(customers_read, text="Cargar/Actualizar Clientes", command=show_customers)
+load_button.grid(row=1, column=0, sticky="ew")
+# ========================================================================================== #
+
+# =================================== CUSTOMERS (UPDATE) =================================== #
+
+# ========================================================================================== #
+
+# =================================== CUSTOMERS (DELETE) =================================== #
+
+# ========================================================================================== #
+
+# ====================================== VALIDACIONES ====================================== #
+def val_customer_id(entry_text):
+    return entry_text.isdigit() and len(entry_text) <= 6 or entry_text == ""
+validation = root.register(val_customer_id)
+C_C_CustomerID.config(validate="key", validatecommand=(validation, "%P"))
+
+def val_nombreapellido(entry_text):
+    return all (char.isalpha() or char.isspace() for char in entry_text) and len(entry_text) <= 20 or entry_text == " " or entry_text == ""
+validation = root.register(val_nombreapellido)
+C_C_Nombre.config(validate="key", validatecommand=(validation, "%P"))
+C_C_Apellido.config(validate="key", validatecommand=(validation, "%P"))
+
+def val_credito(entry_text):
+    pattern = r'^\d{0,7}(\.\d{0,2})?$'
+    return re.match(pattern, entry_text) is not None
+validation = root.register(val_credito)
+C_C_Credito.config(validate="key", validatecommand=(validation, "%P"))
+
+def val_correo(entry_text):
+    return len(entry_text) <= 30 or entry_text == ""
+validation = root.register(val_correo)
+C_C_Correo.config(validate="key", validatecommand=(validation, "%P"))
+
+def val_ingresos(entry_text):
+    return len(entry_text) <= 20 or entry_text == ""
+validation = root.register(val_ingresos)
+C_C_Ingresos.config(validate="key", validatecommand=(validation, "%P"))
+# ========================================================================================== #
+
 root.mainloop()
